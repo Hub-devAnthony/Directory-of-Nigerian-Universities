8cc95767fd8edfcbdac1c50350cf69c023498ccc
iAyomi <ayomitunde.isijola@gmail.com>
2025-10-27 00:31:14 +0100

    changes: Added more fields to DisplayInfo Component

--- src/components/DisplayInfo.jsx
+++ src/components/DisplayInfo.jsx
@@ -1,30 +1,46 @@
-import Text from "./Typography";
+import { Text, GoToLink } from "./Typography";
 
-const DisplayInfo = ({ name, type, state, year, website }) => {
+const DisplayInfo = ({
+  universityName,
+  universityType,
+  universityLocation,
+  yearOfEstablishment,
+  viceChancellor,
+  universityWebsite,
+  universityFaculties,
+  universityDepartments,
+  academicCalendar,
+}) => {
   return (
     <div className="p-5 w-fit m-auto space-y-3 text-left text-lg font-semibold text-black">
       <p>
-        University Name: <Text text={name} />
+        University Name: <Text text={universityName} />
       </p>
       <p>
-        University Type: <Text text={type} />
+        University Type: <Text text={universityType} />
       </p>
       <p>
-        Location: <Text text={state} />
+        Location: <Text text={universityLocation} />
       </p>
       <p>
-        Year of Establishment: <Text text={year} />
+        Year of Establishment: <Text text={yearOfEstablishment} />
       </p>
       <p>
-        Official Website:
-        <a
-          href={website}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-lg font-medium text-blue-600 underline"
-        >
-          {website}
-        </a>
+        Current Vice Chancellor: <Text text={viceChancellor} />
+      </p>
+      <p>
+        Number of Faculties: <Text text={universityFaculties} />
+      </p>
+      <p>
+        Number of Departments: <Text text={universityDepartments} />
+      </p>
+
+      <p>
+        Official Website: <GoToLink link={universityWebsite} />
+      </p>
+
+      <p>
+        Academic Calendar: <GoToLink link={academicCalendar} />
       </p>
     </div>
   );
--- src/components/Typography.jsx
+++ src/components/Typography.jsx
@@ -2,4 +2,17 @@ const Text = ({ text }) => {
   return <span className="text-base font-medium">{text}</span>;
 };
 
-export default Text;
+const GoToLink = ({ link }) => {
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-lg font-medium text-blue-600 underline"
+    >
+      {link}
+    </a>
+  );
+};
+
+export { Text, GoToLink };
